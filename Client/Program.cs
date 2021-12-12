e5c0378f2e9861f69dfe91029ca8eb57b9d93d56
Порховский Максим <maks19801@gmail.com>
2021-12-12 19:42:43 +0200

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+            string data = null;
+            Console.WriteLine("Please, input server ip address...");
+            string ipAddress = Console.ReadLine();
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            Console.WriteLine("Please, input port number");
+            int port = Int32.Parse(Console.ReadLine());
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+            try
+            {
+                while (true)
+                {
+                    Socket client = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.IP);
+                    client.Connect(endPoint);
+                    Console.WriteLine("Input the message: ");
+                    string message = Console.ReadLine();
+
+                    client.Send(Encoding.UTF8.GetBytes(message));
+
+                    int bytesReceived = client.Receive(bytes);
+                    data = Encoding.UTF8.GetString(bytes, 0, bytesReceived);
+                    Console.WriteLine("Received from server: " + data);
+                    if (data == "Bye" || message == "Bye")
+                    {
+                        client.Send(Encoding.UTF8.GetBytes(message));
+                        Console.WriteLine("Client has ended connection...");
+                        break;
+                    }
+
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.IP);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, 1024);
+
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            Console.WriteLine("Server started...");
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Wait for connection through the port {0}", endPoint);
+
+                    Socket handler = socket.Accept();
+
+                    string data = null;
+
+                    byte[] bytes = new byte[1024];
+                    int bytesReceived = handler.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesReceived);
+                    Console.WriteLine("Received from client: " + data);
+
+                    Console.WriteLine("Input the message: ");
+                    string message = Console.ReadLine();
+
+                    handler.Send(Encoding.UTF8.GetBytes(message));
+                    if(data == "Bye" || message == "Bye")
+                    {
+                        Console.WriteLine("Server has ended connection...");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
